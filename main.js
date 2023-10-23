ec70efb36f95a6903f956d2283741eaec74e2609
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-23 22:36:46 +0800

    colors html

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
+//hello js
+const hello = () => {
+  const heading = document.querySelector(".heading1");
+  if (heading.innerText === "Hello") {
+    heading.innerHTML = "Jimzon Pogi";
+  } else {
+    heading.innerHTML = "Hello";
+  }
+};
 document.addEventListener("DOMContentLoaded", function () {
-  //hello js
-  const hello = () => {
-    const heading = document.querySelector(".heading1");
-    if (heading.innerText === "Hello") {
-      heading.innerHTML = "Jimzon Pogi";
-    } else {
-      heading.innerHTML = "Hello";
-    }
-  };
   document.querySelector("#index-btn").onclick = hello;
+});
 
-  //counter js
-  let counter = 0;
-  let count = () => {
-    counter++;
-    document.querySelector(".counth1").innerHTML = counter;
+//counter js
+let counter = 0;
+let count = () => {
+  counter++;
+  document.querySelector(".counth1").innerHTML = counter;
 
-    if (counter % 10 == 0) {
-      alert(`perfect ${counter}`);
-    }
-  };
+  if (counter % 10 == 0) {
+    alert(`perfect ${counter}`);
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("#count-btn").onclick = count;
+});
+
+//form
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("form").onsubmit = function () {
+    const name = document.querySelector("#name").value;
+    alert(`Hello, ${name}`);
+  };
 });
