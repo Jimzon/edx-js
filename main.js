6a1177c22639f2ed9b9e47628546542b1176cf74
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-23 16:41:27 +0800

    counter

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const hello = () => {
   alert("Jimzon pogi");
 };
+
+let counter = 0;
+
+let count = () => {
+  counter++;
+  alert(counter);
+};
+
+// document.querySelector('#count-btn').addEventListener(onclick {
+//     counter++;
+// })
