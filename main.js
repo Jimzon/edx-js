fce08a04dc8d788d52f0ac46a3e852077f64f9f9
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-23 23:23:52 +0800

    color function

--- main.js
+++ main.js
@@ -32,3 +32,16 @@ document.addEventListener("DOMContentLoaded", function () {
     alert(`Hello, ${name}`);
   };
 });
+
+//color
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#red").onclick = function () {
+    document.querySelector("#hello").style.color = "red";
+  };
+  document.querySelector("#blue").onclick = function () {
+    document.querySelector("#hello").style.color = "blue";
+  };
+  document.querySelector("#green").onclick = function () {
+    document.querySelector("#hello").style.color = "green";
+  };
+});
