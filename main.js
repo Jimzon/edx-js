25f6cad22164242ce2ceaa3a1a111a06ee65af93
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-24 00:25:39 +0800

    select option

--- main.js
+++ main.js
@@ -35,9 +35,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //color
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("button").forEach(function (button) {
-    button.onclick = function () {
-      document.querySelector("#hello").style.color = button.dataset.color;
-    };
-  });
+  //   document.querySelectorAll("button").forEach(function (button) {
+  //     button.onclick = function () {
+  //       document.querySelector("#hello").style.color = button.dataset.color;
+  //     };
+  document.querySelector("select").onchange = function () {
+    document.querySelector("#hello").style.color = this.value;
+  };
 });
